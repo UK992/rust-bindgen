2d2f7133093e70cf79bbd693ed925fb7c898a3cf
UK992 <urbankrajnc92@gmail.com>
2017-07-27 02:02:31 +0200

    test4

--- ci/test.py
+++ ci/test.py
@@ -25,7 +25,6 @@ print('Checking for #include directives of system headers...')
 include_found = False
 headers_dir = os.path.join(sys.path[0], os.pardir, 'tests', 'headers')
 for f in os.listdir(headers_dir):
-    #print(f)
     h_file = os.path.join(headers_dir, f)
     with open(h_file, 'r') as content:
         for idx, line in enumerate(content.readlines()):
@@ -46,17 +45,17 @@ if include_found:
     sys.exit(msg)
 else:
     print('Found none; OK!')
-        #for line in infile:
-        #    if line.startswith(txt):
-        #        line = line[0:len(txt)] + ' - Truly a great person!\n'
-        #    outfile.write(line)
-#   line = re.findall(r'#include\s*<.*>', line)[0]
 
 #steps
+cwd_path = os.getcwd()
 with open(os.path.join(sys.path[0], 'test.commands'), 'r') as steps:
     for line in steps.readlines():
+        line = line.strip()
         if line.strip().startswith('#') or not line.strip():
             continue
+
+        if line.strip().startswith('cd '):
+            cwd_path = os.path.normpath(os.path.join(cwd_path, line.split(' ')[1]))
 
         if '%no_diff_assert%' in line:
             no_diff_assert()
@@ -69,13 +68,14 @@ with open(os.path.join(sys.path[0], 'test.commands'), 'r') as steps:
                 line = line.replace(env, os.environ.get(env[4:-1], ''))
 
         print(line)
-        subprocess.check_call(line, shell=True)
+        print(cwd_path)
+        subprocess.Popen(line, shell=True, cwd=cwd_path)
 
 
 def no_diff_assert():
     # git add -u
     # git diff @
     # git diff-index --quiet HEAD
-    subprocess.check_call('git add -u',shell=True)
-    subprocess.check_call('git diff @',shell=True)
-    subprocess.check_call('git diff-index --quiet HEAD',shell=True)
+    subprocess.check_call('git add -u', shell=True)
+    subprocess.check_call('git diff @', shell=True)
+    subprocess.check_call('git diff-index --quiet HEAD', shell=True)
